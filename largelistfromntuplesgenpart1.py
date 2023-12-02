056ec0113f6e2cd86951237f47904ecfc5e8831b
Nonpercents <32957431+Nonpercents@users.noreply.github.com>
2023-12-02 23:53:02 +0900

    Tetrational level

--- largelistfromntuplesgenpart1.py
+++ largelistfromntuplesgenpart1.py
@@ -1,3 +1,5 @@
+#Growth rate: approx. tetration level
+
 j=2
 while True:
     a=eval(str("[("+"{},"*(j-1)+"{}) for {} in range({})"+" for {} in range(1,{}+1)"*(j-1)+"]").format(*["v"+str(i) for i in range(j)],"v0",str(j+1),*["v"+str(i//2*3-i+1) for i in range(j*2-2)]))
